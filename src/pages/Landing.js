c6e707fb70c9ad2031f5ac444a619b140e9e1e21
Priyadarsh S S <priyadarsh2001@gmail.com>
2023-10-08 01:41:56 +0530

    Setup

---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+'use client'
+import { Text } from '@chakra-ui/react';
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div>
+      <Text color="black">Home</Text>
+    </div>
+  )
+}
+
+export default Landing;
