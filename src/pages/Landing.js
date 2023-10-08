7ff8ec7d747d0e4db7c0b449ddce7adf65ad32d5
Priyadarsh S S <priyadarsh2001@gmail.com>
2023-10-08 17:51:17 +0530

    Hotfix

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -29,29 +29,6 @@ const HeroSection = () => {
           justifyContent='center'
           maxW='480px'
         >
-          {/* <HStack
-            as={Link}
-            p={1}
-            rounded='full'
-            fontSize='sm'
-            w='max-content'
-            bg={useColorModeValue('gray.300', 'gray.700')}
-          >
-            <Box
-              py={1}
-              px={2}
-              lineHeight={1}
-              rounded='full'
-              color='white'
-              bgGradient='linear(to-l, #0ea5e9,#2563eb)'
-            >
-              What&apos;s new
-            </Box>
-            <HStack spacing={1} alignItems='center' justifyContent='center'>
-              <Text lineHeight={1}>See our recent updates</Text>
-              <Icon as={GoChevronRight} w={4} h={4} />
-            </HStack>
-          </HStack> */}
           <chakra.h1
             fontSize='5xl'
             lineHeight={1}
@@ -116,7 +93,6 @@ const HeroSection = () => {
           </HStack>
         </Stack>
         <Box ml={{ base: 0, md: 5 }} pos='relative'>
-          <DottedBox />
           <Image
             w='100%'
             h='100%'
@@ -132,44 +108,6 @@ const HeroSection = () => {
         </Box>
       </Stack>
     </Container>
-  )
-}
-
-function DottedBox() {
-  return (
-    <Box
-      position='absolute'
-      left='-45px'
-      top='-30px'
-      height='full'
-      maxW='700px'
-      zIndex={-1}
-    >
-      <svg
-        color='gray.200'
-        width='350'
-        height='420'
-        fill='none'
-      >
-        <defs>
-          <pattern
-            id='5d0dd344-b041-4d26-bec4-8d33ea57ec9b'
-            x='0'
-            y='0'
-            width='20'
-            height='20'
-            patternUnits='userSpaceOnUse'
-          >
-            <rect x='0' y='0' width='4' height='4' fill='currentColor'></rect>
-          </pattern>
-        </defs>
-        <rect
-          width='404'
-          height='404'
-          fill='url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)'
-        ></rect>
-      </svg>
-    </Box>
   )
 }
 
